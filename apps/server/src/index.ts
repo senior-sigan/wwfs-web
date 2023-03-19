79e4e7bbd695e197a2b07da5c61bde0ba689dd49
Ilya Siganov <ilya.blan4@gmail.com>
2023-03-19 16:12:29 +0600

    add code to create a room

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,43 @@
 import { randomUUID } from "crypto";
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
+
+type Player = WebSocket;
+
+type Room = {
+  players: Player[];
+  started: boolean;
+};
 
 const wss = new WebSocketServer({ port: 3001 });
+const MAX_PLAYERS_IN_ROOM = 2;
+const rooms: Array<Room> = [];
+
+function newRoom(ws: WebSocket) {
+  return {
+    players: [ws],
+    started: false,
+  };
+}
+
+function findOrCreateRoom(ws: WebSocket) {
+  const lastRoom = rooms[rooms.length - 1];
+  if (rooms.length !== 0 && !lastRoom.started) {
+    lastRoom.players.push(ws);
+    if (lastRoom.players.length >= MAX_PLAYERS_IN_ROOM) {
+      lastRoom.started = true;
+    }
+    return lastRoom;
+  }
+
+  const room = newRoom(ws);
+  rooms.push(newRoom(ws));
+  return room;
+}
 
 wss.on("connection", (ws) => {
-  const id = randomUUID();
+  const uid = randomUUID();
+
+  const room = findOrCreateRoom({ws, uid});
 
   const timer = setInterval(() => {
     ws.send("PING");
@@ -22,6 +55,7 @@ wss.on("connection", (ws) => {
   ws.on("close", () => {
     clearInterval(timer);
     console.log(`CLOSE: id=${id}`);
+    room.players.
   });
 
   ws.send("HELLO");
